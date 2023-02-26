477ed75ba9037685b6e3c3765dadd5b906cd8171
Asmaak83 <asmaa.khaled.3394@gmail.com>
2023-02-26 19:49:12 +0200

    add using vue version

---
+++ with_vue/zid/src/main.js
@@ -0,0 +1,25 @@
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+import "bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./scss/main.scss";
+import "normalize.css";
+
+
+/* import the fontawesome core */
+import { library } from '@fortawesome/fontawesome-svg-core'
+
+/* import font awesome icon component */
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+/* import specific icons */
+import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
+
+/* add icons to the library */
+library.add(faUserSecret)
+
+
+
+createApp(App).use(store).use(router).component('font-awesome-icon', FontAwesomeIcon).mount("#app");
